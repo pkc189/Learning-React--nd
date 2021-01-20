393f9f30d35700e3da4f2f9a209e87944e102906
pkc189 <pankajchauhan189@gmail.com>
2021-01-20 23:42:37 +0530

    3 files modified

--- src/Color.js
+++ src/Color.js
@@ -1,7 +1,12 @@
-export default function Color({ title, color, rating }) {
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+export default function Color({ id, title, color, rating, onRemove = f => f }) {
   return (
     <section>
       <h1>{title}</h1>
+      <button onClick={() => onRemove(id)}>
+        <FaTrash />
+      </button>
       <div style={{ height: 50, backgroundColor: color }} />
     </section>
   );
--- src/Rating.js
+++ src/Rating.js
@@ -4,7 +4,15 @@ import ColorList from "./ColorList.js";
 import { FaStar } from "react-icons/fa";
 
 export default function StarRating() {
-  const [colors] = useState(colorData);
+  const [colors, setColors] = useState(colorData);
 
-  return <ColorList colors={colors} />;
+  return (
+    <ColorList
+      colors={colors}
+      onRemoveColor={id => {
+        const newColors = colors.filter(color => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  );
 }
