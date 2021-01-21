390937091d3b9def92adc3bae2feec4935f359df
pkc189 <pankajchauhan189@gmail.com>
2021-01-21 09:42:14 +0530

    2 files modified

--- src/Rating.js
+++ src/Rating.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from "react";
-import colorData from "./color-data.json";
-import ColorList from "./ColorList.js";
-import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  const [colors, setColors] = useState(colorData);
+
+export default function Checkbox (){
+  const [checked, setChecked] = useState(false);
+
+
 
   return (
-    <ColorList
-      colors={colors}
-      onRemoveColor={id => {
-        const newColors = colors.filter(color => color.id !== id);
-        setColors(newColors);
-      }}
-    />
+    <>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked(checked => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
+    </>
   );
-}
+};
